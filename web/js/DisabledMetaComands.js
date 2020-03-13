36676ef77c08f541e374bff6234d18e247668dc4
AvatarAang30 <1kv.diaz.moreno.rogelio@gmail.combffgbgfb>
2020-03-13 00:04:41 -0600

    Modificacion del tamao de fuente en la interfaz de Registrar

--- web/js/DisabledMetaComands.js
+++ web/js/DisabledMetaComands.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
     //Disable cut copy paste
     $('body').bind('cut copy paste', function (e) {
         e.preventDefault();
+        alert("Accion no permitida");
     });
    
     //Disable mouse right click
     $("body").on("contextmenu",function(e){
+        alert("Accion no permitida");
         return false;
     });
 });
