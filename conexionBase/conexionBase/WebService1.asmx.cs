c525326e3b26e9516439d2b9c8450273ca441626
AvatarAang30 <1kv.diaz.moreno.rogelio@gmail.combffgbgfb>
2020-04-17 16:42:54 -0500

    Servicio web con conexion a la base de datos del proyecto

---
+++ conexionBase/conexionBase/WebService1.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using Npgsql;
+using System.Text;
+
+namespace conexionBase
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public void GetData()
+        {
+            NpgsqlConnection conexion = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["postgres"].ConnectionString);
+            conexion.Open();
+            NpgsqlCommand comand = new NpgsqlCommand("SELECT * FROM personas", conexion);
+            NpgsqlDataAdapter ad = new NpgsqlDataAdapter(comand);
+            DataSet ds = new DataSet();
+            ad.Fill(ds);
+            //return ds;
+        }
+    }
+}
